f71b0e9784032e79ae7e3ee52b26a417c702ff29
amyshkin <alexander.myshkin@sarov-itc.ru>
2020-05-15 20:58:04 +0300

    Added first option application

---
+++ modules/template_heap/application/template_heap_app.cpp
@@ -0,0 +1,29 @@
+// Copyright 2020 Myshkin Andrey
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#include "include\template_heap_app.h"
+
+//int main(int argc, const char** argv) {
+//	printf("Hello World");
+//	return 0;
+//}
+
+int main(int argc, const char** argv) {
+    Heap_App App;
+    std::string output = App(argc, argv);
+    printf("%s\n", output.c_str());
+    return 0;
+}
+
+
+//int main(int argc, const char** argv) {
+//    Heap_App App;
+//
+//    std::cout << App(argc, argv);
+//
+//    return 0;
+//}
