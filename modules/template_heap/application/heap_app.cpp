641ea03b744861144b505756cc43f71c4c848aab
amyshkin <alexander.myshkin@sarov-itc.ru>
2020-05-21 21:40:46 +0300

    Fixed by manager

--- modules/template_heap/application/heap_app.cpp
+++ modules/template_heap/application/heap_app.cpp
@@ -1,16 +1,13 @@
 // Copyright 2020 Myshkin Andrey
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <string>
 #include <iostream>
 
 #include "include/template_heap_app.h"
 
-
 int main(int argc, const char** argv) {
     Heap_App App;
     std::string output = App(argc, argv);
-    printf("%s\n", output.c_str());
+    std::cout << output << std::endl;
     return 0;
 }
